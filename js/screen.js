95bd8ef4f4a09d6952318ba32f8a1d16f2449b25
Ebahena2 <89425354+Ebahena2@users.noreply.github.com>
2023-10-12 22:56:34 -0500

    played around with the css and js and added multiple pop up screens

--- js/screen.js
+++ js/screen.js
@@ -1,16 +1,16 @@
-jQuery(function($)){
-  $('html').removeClass('nojs')
-  $('html').addClass('hasjs')
- 
- 
-  $(form).ready(function(){
-  $(#submitButton).click(function(){
-  if(confirm("Are you Sure?")){
-  alert("You have been enrolled into the raffle!");
-  } else alert("Enrollment was not completeed");
+jQuery(function($){
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
   
-  }
-  }
+});
+ 
+ 
+  $(document).ready(function(){
+    $("#submit").click(function(){
+      if(confirm("Are you ready to enter the Raffle?")){
+      alert("You have been enrolled into the raffle!");}
+      else alert("Enrollment has not worked");
+  
+  });
+  });
 
-
-}
